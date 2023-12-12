d818f1cf9a13cfd2ed8133316394678f571c032e
ArtemAleksandrovich <temikzhavoronkov@mail.ru>
2023-12-12 18:42:30 +0300

    fix

--- src/main/java/tnt/evoscada/pluginformavensettings/SetMavenSettingsStartupActivity.java
+++ src/main/java/tnt/evoscada/pluginformavensettings/SetMavenSettingsStartupActivity.java
@@ -10,20 +10,19 @@ import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
 
 public class SetMavenSettingsStartupActivity implements com.intellij.openapi.startup.StartupActivity {
-    private static final Key<Boolean> ALREADY_EXECUTED_FLAG = Key.create("SetVMOptionsExecuted");
 
     @Override
     public void runActivity(@NotNull Project project) {
-        if (project.getUserData(ALREADY_EXECUTED_FLAG) == null) {
-            var mavenRunnerSettings = MavenRunner.getInstance(project).getState();
-            MavenProjectsManager mavenProjectsManager = ServiceManager.getService(project, MavenProjectsManager.class);
 
-            if (mavenProjectsManager != null && mavenProjectsManager.isMavenizedProject()) {
-                var vmOptions = "-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true";
-                mavenProjectsManager.getImportingSettings().setVmOptionsForImporter(vmOptions);
-                mavenRunnerSettings.setVmOptions(vmOptions);
-            }
-            project.putUserData(ALREADY_EXECUTED_FLAG, true);
+        var mavenRunnerSettings = MavenRunner.getInstance(project).getState();
+        MavenProjectsManager mavenProjectsManager = ServiceManager.getService(project, MavenProjectsManager.class);
+
+        if (mavenRunnerSettings.getVmOptions().isEmpty() && mavenProjectsManager.getImportingSettings().getVmOptionsForImporter().isEmpty() &&
+                mavenProjectsManager != null && mavenProjectsManager.isMavenizedProject()) {
+
+            var vmOptions = "-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true";
+            mavenProjectsManager.getImportingSettings().setVmOptionsForImporter(vmOptions);
+            mavenRunnerSettings.setVmOptions(vmOptions);
         }
     }
 }
